3f25916f72192bfae7ae39011894ac7c96bdb41f
Rob Hudson <rob@cogit8.org>
2008-03-07 22:46:47 -0800

    misc updates while getting views working.

--- codereviewr/code/urls.py
+++ codereviewr/code/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
+from codereviewr.code.views import code_list, code_detail
 
-urlpatterns = patterns('codereviewr.code.views',
-    # Writes
-    url(r'^(?P<code_id>\d+)/', 'code_detail', name='code_detail'),
+urlpatterns = patterns('',
+    url(r'^$', code_list, name='code_list'),
+    url(r'^(?P<code_id>\d+)/$', code_detail, name='code_detail'),
 )

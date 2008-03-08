ec4b8fb85386b68834f9514defd2409d9fa8a602
Rob Hudson <rob@cogit8.org>
2008-03-07 19:57:52 -0800

    start project

---
+++ codereviewr/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^codereviewr/', include('codereviewr.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)

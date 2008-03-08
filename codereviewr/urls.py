363bc29b5a9afb5cce0531b2c2a854d72d07dc3e
David Merwin <dave@david-merwins-powerbook-g4-22.local>
2008-03-07 21:32:01 -0800

    added local homepage to urls

--- codereviewr/urls.py
+++ codereviewr/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template, redirect_to
 
 urlpatterns = patterns('',
      (r'^admin/', include('django.contrib.admin.urls')),
+     
+     #for homepage - testing
+     (r'^$', direct_to_template, {'template': 'homepage.html'}),
+     
 )

6de4006c52d757713321b98f64527240458c000e
Rob Hudson <rob@cogit8.org>
2008-03-08 00:11:20 -0800

    Updating urls to not depend on path

--- codereviewr/urls.py
+++ codereviewr/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template, redirect_to
-from codereviewr.settings import DEBUG
+from codereviewr.settings import PROJECT_PATH, DEBUG
 import os
 
 urlpatterns = patterns('',
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
 
 if DEBUG:
     urlpatterns += patterns('', 
-        (r'^media/(.*)$', 'django.views.static.serve', {'document_root': os.path.expanduser('~/sandbox/codereviewr/media')}),
+        (r'^media/(.*)$', 'django.views.static.serve', {'document_root': '%s/../media' % (PROJECT_PATH)})
     )

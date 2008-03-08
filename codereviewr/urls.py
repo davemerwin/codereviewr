4be02cf92ae1dcdf4a25271f0e4eb58e47e3a03c
David Merwin <dave@david-merwins-powerbook-g4-22.local>
2008-03-07 21:48:46 -0800

    add in templates base and start setting up the templates

--- codereviewr/urls.py
+++ codereviewr/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template, redirect_to
+from codereviewr.settings import DEBUG
+import os
 
 urlpatterns = patterns('',
      (r'^admin/', include('django.contrib.admin.urls')),
@@ -8,3 +10,8 @@ urlpatterns = patterns('',
      (r'^$', direct_to_template, {'template': 'homepage.html'}),
      
 )
+
+if DEBUG:
+    urlpatterns += patterns('', 
+        (r'^media/(.*)$', 'django.views.static.serve', {'document_root':os.path.expanduser('~/sandbox/codereviewr/media')}),
+    )

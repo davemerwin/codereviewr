9d76ee5189261614b0b1ee966a6720a2ae45e17d
Rob Hudson <rob@cogit8.org>
2008-03-07 21:18:51 -0800

    Start of code model

--- codereviewr/code/models.py
+++ codereviewr/code/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Code(models.Model):
+    """
+    Core code model for code snippets
+    """
+    title = models.CharField(max_length=200)
+    code = models.TextField(help_text="")
+    author = models.ForeignKey(User)
+    description = models.TextField(blank=True)
+    dependencies = models.CharField(blank=True, max_length=255)
+    # language ... get from Pygments
+    version = models.CharField(blank=True, max_length=100)
+    is_public = models.BooleanField(default=True)
+    created = models.DateTimeField(default=datetime.now)
+    updated = models.DateTimeField(blank=True)
+
+    def __unicode__(self):
+        return "%s by %s" % (self.title, self.author.get_name())
+
+    class Admin:
+        list_display = ('title','author','is_public','created')
--- codereviewr/settings.py
+++ codereviewr/settings.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'codereviewr.code',
 )
 
 # For local development setting overrides

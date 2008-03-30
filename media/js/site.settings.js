471b1c8f68210c810a633b901e8d76c6f90aede9
David Merwin <dave@david-merwins-powerbook-g4-22.local>
2008-03-29 21:52:58 -0700

    change design code v2

--- media/js/site.settings.js
+++ media/js/site.settings.js
@@ -5,6 +5,13 @@
  *
  */
  $(document).ready(function(){
+     $('.rounded a').corner({
+            tl: { radius: 5 },
+            tr: { radius: 5 },
+            bl: { radius: 5 },
+            br: { radius: 5 },
+            validTags: ['a'],
+        });
     $('.close').click(function() {
         $(this).parent.close();
         $(this).parent.('.loginout').toggle();
